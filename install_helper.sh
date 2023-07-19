19cb2e8ce5574478968957c6a95cfc144eabd898
JP Robinson <jp.robinson@datadoghq.com>
2023-07-19 18:20:57 +0000

    pip, git, nvim

--- install_helper.sh
+++ install_helper.sh
@@ -6,6 +6,8 @@ set -x
 sudo apt-get update && sudo apt-get -q -y upgrade
 sudo apt-get -q -y install tmux htop curl neovim python3-neovim
 
+pip3 install neovim
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -13,14 +15,17 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sh -c "$(curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
 
-/usr/bin/nvim +PlugInstall +qall
+nvim +PlugInstall +qall
 
 # bring in the dot files
-mv ~/.config ~/.config.bak
-mv ~/home/.config ~/.config
+mv ~/.config/nvim ~/.config-nvim.bak
+mv ~/home/.config/nvim ~/.config/nvim
 
 mv ~/.zshrc ~/.zshrc.bak
 mv ~/home/.zshrc ~/.zshrc
 
 mv ~/.gitconfig ~/.gitconfig.bak
 mv ~/home/.gitconfig ~/.gitconfig
+
+git config --global user.name JP Robinson
+git config --global user.email jp.robinson@datadoghq.com
