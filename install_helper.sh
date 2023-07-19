86a39c99406d6339c3f454e0e92de516a894c23d
JP Robinson <jp.robinson@datadoghq.com>
2023-07-19 11:53:19 -0400

    install curl first maybe

--- install_helper.sh
+++ install_helper.sh
@@ -2,15 +2,15 @@
 set -e
 set -x
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # install neovim
 sudo apt-get update && sudo apt-get -q -y upgrade
 sudo apt-get install tmux htop curl neovim python3-neovim
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "$(curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
 
 /usr/bin/nvim +PlugInstall +qall
