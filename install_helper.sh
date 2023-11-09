5ae27ceb59bf115fea128d58ac00b9842b8f8644
JP Robinson <jp.robinson@datadoghq.com>
2023-11-09 12:58:03 -0500

    moving ohmyzsh to the top, temp git config swap

--- install_helper.sh
+++ install_helper.sh
@@ -8,11 +8,17 @@ sudo apt-get -q -y install tmux htop curl neovim python3-neovim direnv
 
 pip3 install neovim
 
-# install vim-plug
+echo "installing oh-my-zsh"
+# force to https for the install, ssh isnt setup at this point
+git config --global url."https://github".insteadOf git://github
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+# reset the git insteadOf settings for Go
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+
+echo "installing vim-plug"
 sh -c "$(curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
 
-# bring in the dot files
 echo "copying dot files"
 cp -R ~/dotfiles/.config/nvim ~/.config/nvim
 
@@ -35,8 +41,5 @@ echo "configuring git"
 git config --global user.name JP Robinson
 git config --global user.email jp.robinson@datadoghq.com
 git config --global pull.rebase true
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
 sudo ddtool docker install-helper
