8708f1c8912fdfc8661a42dd160738d0174c12d9
JP Robinson <jp.robinson@datadoghq.com>
2023-11-09 10:21:39 -0500

    attempting a few updates

--- install_helper.sh
+++ install_helper.sh
@@ -12,7 +12,6 @@ pip3 install neovim
 sh -c "$(curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
 
-
 # bring in the dot files
 echo "copying dot files"
 cp -R ~/dotfiles/.config/nvim ~/.config/nvim
@@ -23,6 +22,12 @@ echo "initiating nvim"
 
 nvim +PlugInstall +qall
 
+nvim +UpdateRemotePlugins
+
+echo "installing gocode for vim-go"
+
+go install github.com/nsf/gocode@latest
+
 echo "configuring git"
 
 git config --global user.name JP Robinson
@@ -30,5 +35,8 @@ git config --global user.email jp.robinson@datadoghq.com
 git config --global pull.rebase true
 
 source ~/.zshrc
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
 sudo ddtool docker install-helper
